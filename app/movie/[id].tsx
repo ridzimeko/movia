9e308ee2fcad05ca72e6adf30a9c299033130d3d
Amro Alfien <ridzimeko@proton.me>
2025-04-10 07:00:25 +0000

    feat: navbar menu & assets

---
+++ app/movie/[id].tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { useLocalSearchParams } from "expo-router";
+
+const MovieDetail = () => {
+  const { id } = useLocalSearchParams();
+  return (
+    <View>
+      <Text>Movie details: {id}</Text>
+    </View>
+  );
+};
+
+export default MovieDetail;
+
+const styles = StyleSheet.create({});
