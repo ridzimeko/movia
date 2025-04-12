1c6c06d3e50cebeba08519c7d1023fecd16ce6c4
Amro Alfien <ridzimeko@proton.me>
2025-04-12 04:40:05 +0000

    feat: slicing home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,29 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { useRouter } from "expo-router";
+import { Image, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
+  const router = useRouter();
+
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-2xl font-bold tracking-tight">Home Screen</Text>
-      <Link href="/movie/avenger">Avenger</Link>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <ScrollView
+        className="z-10 px-5"
+        showsVerticalScrollIndicator={true}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Image source={icons.logo} className="w-12 h-auto mx-auto mt-20 mb-5" />
+
+        <View>
+          <SearchBar
+            placeholder="Search for a movie"
+            onPress={() => router.push("/search")}
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
