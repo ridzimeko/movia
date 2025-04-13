ef57533bf385c419093647d48351a8450fc5621a
Amro Alfien <ridzimeko@proton.me>
2025-04-13 13:47:33 +0000

    feat: appwrite setup & movie metrics

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -6,6 +6,7 @@ import { fetchMovies } from "@/services/api";
 import MovieCard from "@/components/MovieCard";
 import { icons } from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
+import { updateSearchCount } from "@/services/appwrite";
 
 const search = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -27,6 +28,10 @@ const search = () => {
     const timeoutId = setTimeout(async () => {
       if (searchQuery.trim()) {
         await loadMovies();
+
+        if (movies?.length > 0 && movies?.[0]) {
+          updateSearchCount(searchQuery, movies[0]);
+        }
       } else {
         resetMovies();
       }
---
+++ services/appwrite.ts
@@ -0,0 +1,42 @@
+import { Client, Databases, ID, Query } from "react-native-appwrite";
+const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
+const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
+
+const client = new Client()
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!)
+  .setPlatform("id.my.slayerwitch.movia");
+
+const database = new Databases(client);
+
+export const updateSearchCount = async (query: string, movie: Movie) => {
+  try {
+    // check a record of the search
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.equal("search_term", query),
+    ]);
+
+    if (result.documents.length > 0) {
+      const existingMovie = result.documents[0];
+
+      await database.updateDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        existingMovie.$id,
+        {
+          count: existingMovie.count + 1,
+        }
+      );
+    } else {
+      await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+        search_term: query,
+        movie_id: movie.id,
+        count: 1,
+        title: movie.title,
+        poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
